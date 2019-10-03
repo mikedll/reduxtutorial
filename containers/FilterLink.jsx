ad20c6899f93b01f7572acf395ff131519ef8ec2
Michael Rivera <soymrmike@gmail.com>
2019-10-03 06:09:51 -0700

    Fixed missing dispatch call.

--- containers/FilterLink.jsx
+++ containers/FilterLink.jsx
@@ -12,7 +12,7 @@ const mapStateToProps = (state, ownProps)  => {
 const mapDispatchToProps = (dispatch, ownProps) => {
   return {
     onClick: () => {
-      setVisibilityFilter(ownProps.filter)
+      dispatch(setVisibilityFilter(ownProps.filter))
     }
   }
 }
